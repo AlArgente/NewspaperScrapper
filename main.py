f60d429019d73874d9e84964ea1562c762553b4b
AlArgente <albertoargentedcgarrido@gmail.com>
2021-10-14 18:34:37 +0200

    Updated year and name in LICENSE and .py files.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # MIT License
-# Copyright (c) [year] [fullname]
+# Copyright (c) 2021 Alberto Argente del Castillo Garrido
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -35,7 +35,7 @@ Github: AlArgente
 """
 
 
-from news_factory import NewsFactory
+from scrapper.news_factory import NewsFactory
 
 def print_available_newspaper_scrappers():
     """Function to print the available newspaper scrappers.
